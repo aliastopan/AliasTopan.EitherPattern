ee94769ddbb09bb9c0fd2acc9c211f86e49c4990
Taufan Augusta <taufan.augusta@protonmail.com>
2025-09-06 22:49:33 +0700

    chore: drop obsolete

--- tests/AliasTopan.EitherPattern.ManualTest/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using AliasTopan.EitherPattern.ManualTest.Tests;
-
-EitherTest.RunTest();
-NonGenericSuccessTypeTest.RunTest();
-TransformTest.RunTest();
-ThenPatternTest.Run();
-await ThenPatternAsyncTest.RunTest();
-await TransformAsyncTest.RunTest();
-LazyEvaluationPatternTest.RunTest();
